f5ae0de12b1a63140ba777b14fd711a94b2ae0af
Melih Yaşar SÖZEN <mlhysrszn@gmail.com>
2021-09-08 15:48:18 +0300

    Fragments added

--- app/src/main/java/com/mlhysrszn/newsapp/MainActivity.kt
+++ app/src/main/java/com/mlhysrszn/newsapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.mlhysrszn.newsapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.mlhysrszn.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        val detailFragment = NewsDetailFragment.newInstance("MainActivity", "")
+//
+//        val fragmentTransaction = supportFragmentManager.beginTransaction()
+//        fragmentTransaction.replace(R.id.frag1, detailFragment).commit()
     }
 }
